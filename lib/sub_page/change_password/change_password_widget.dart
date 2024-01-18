da8a101e60dcd0a0a01a5714ffdb9be59b3d2ca1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-18 01:02:39 +0000

    Updating to latest FlutterFlow output.

--- lib/sub_page/change_password/change_password_widget.dart
+++ lib/sub_page/change_password/change_password_widget.dart
@@ -5,7 +5,6 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'dart:async';
 import '/custom_code/actions/index.dart' as actions;
 import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
@@ -299,11 +298,6 @@ class _ChangePasswordWidgetState extends State<ChangePasswordWidget> {
                         await authManager.signOut();
                         GoRouter.of(context).clearRedirectLocation();
 
-                        unawaited(
-                          () async {
-                            await actions.onesignalLogout();
-                          }(),
-                        );
                         setState(() {
                           FFAppState().deleteUserInfo();
                           FFAppState().UserInfo =
